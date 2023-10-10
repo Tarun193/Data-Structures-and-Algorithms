3ebae86f4a4a1a425b92bfb1b3de692be928318b
Tarun Chawla <65712331+Tarun193@users.noreply.github.com>
2023-10-10 18:25:47 -0400

    Implemented Basic Strcutres of linkedlist node.

---
+++ Python/LinkedList/LinkedList.py
@@ -0,0 +1,15 @@
+# In this I am going to create a Basic LinkedList Structure
+# After that I will implement operations on that list.
+
+# Basic Structure for node of Linked List.
+class Node:
+    # Constructor for Node
+    # While creating node, the next of node can be null.
+    def __init__(self, value, next = None) -> None:
+        self.value = value
+        self.next  = next
+    
+    # This methods gives the String repersentation of the object
+    # So when even some will call print method on the Node object It will print node's value
+    def __str__(self) -> str:
+        return str(self.value)
