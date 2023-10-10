2055e2bc8fb4d316cf62be428d1b96a7a70f0500
Tarun Chawla <65712331+Tarun193@users.noreply.github.com>
2023-10-10 18:48:57 -0400

    implemented Baic LinkedList class

--- Data Structures/LinkedList/LinkedList.py
+++ Data Structures/LinkedList/LinkedList.py
@@ -3,15 +3,46 @@
 
 # Basic Structure for node of Linked List.
 class Node:
-    # Constructor for Node
-    # While creating node, the next of node can be null.
+    
     def __init__(self, value, next = None) -> None:
+        """
+        Initialize a Node with a value and an optional reference to the next node.
+
+        :param value: The value to store in the node.
+        :param next_node: A reference to the next node in the linked list.
+        """
         self.value = value
         self.next  = next
     
-    # This methods gives the String repersentation of the object
-    # So when even some will call print method on the Node object,
-    # It will print node's value
-    # For example print(Node(4)) -> 4
     def __str__(self) -> str:
+        """
+        Return a string representation of the node's value.
+        example: print(Node(4)) -> 4
+        """
         return str(self.value)
+    
+
+# Structures for LinkedList
+class LinkedList:
+    def __init__(self, head = None) -> None:
+        """
+        Initialize a linked list with an optional head node.
+        :param head: The head node of the linked list.
+        """
+        self.head = head
+
+    def __str__(self) -> str:
+        """
+        Return a string representation of the LinkedList.
+        """
+        temp = self.head
+        list_string = ''
+        while temp:
+            list_string += str(temp.value) # Convert the value to a string
+            if temp.next: # adding a separator
+                list_string += "-->"
+            temp = temp.next
+        return list_string
+
+
+print(LinkedList(Node(4, Node(5, Node(6)))))
